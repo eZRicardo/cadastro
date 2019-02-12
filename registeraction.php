0c596e7d7ea02f79c20e58d27fb4a07be8abafd6
José Ricardo <josericardo@polijuniorengenharia.com.br>
2019-02-12 09:31:49 -0300

    Bug do cadastro resolvido

--- registeraction.php
+++ registeraction.php
@@ -5,8 +5,7 @@ $nome = $_POST['nome'];
 $senha = $_POST['senha'];
 $cpf = $_POST['cpf'];
 
-$consulta = "SELECT cpf FROM cliente WHERE cpf='$cpf'";
-
+$consulta = "SELECT cpf FROM cadastrar WHERE cpf='$cpf'";
 $check = $conn->query($consulta);
 $check->fetch_assoc();
 
@@ -21,13 +20,15 @@ die("CPF já cadastrado no sistema");
 }
 
 else{
-$inserirdados = "INSERT INTO cadastro (nome,senha,cpf) VALUES ('$nome','$senha','$cpf') ";
+$inserirdados = "INSERT INTO cadastrar (nome,senha,cpf) VALUES ('$nome','$senha','$cpf') ";
 $result = $conn->query($inserirdados);
 
-if($inserirdados){
+if($result){
     die("Seja bem-vindo");
+    header("location: html/login.html");
 }
  else{
+     echo ($inserirdados);
     die("Infelizmente não foi possível realizar seu cadastro");
 }
 }
